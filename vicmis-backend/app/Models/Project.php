bb02c90d7239e44eabc83cf57c682d4400adb812
conicles <nicolasvillarante@gmail.com>
2026-04-23 23:06:32 +0800

    logs project.php

--- vicmis-backend/app/Models/Project.php
+++ vicmis-backend/app/Models/Project.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes; 
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\LogsActivity;
 
 class Project extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use LogsActivity;
 
     protected $fillable = [
         'lead_id',
