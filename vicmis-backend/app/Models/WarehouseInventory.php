f81cc6768a3c066e8e9679d5381ccfefdfd2da7e
conicles <nicolasvillarante@gmail.com>
2026-04-23 23:03:43 +0800

    logs warehouseinventory.php

--- vicmis-backend/app/Models/WarehouseInventory.php
+++ vicmis-backend/app/Models/WarehouseInventory.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\LogsActivity;
 
 class WarehouseInventory extends Model
 {
     use HasFactory;
     use HasFactory, SoftDeletes;
+    use LogsActivity;
 
     protected $table = 'warehouse_inventory';
 
