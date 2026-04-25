5270097da097e94ca398f2b9a7fe3ec2aaa63a04
conicles <nicolasvillarante@gmail.com>
2026-04-25 11:39:31 +0800

    backtonormaluserupdatedepartment

--- vicmis-backend/database/migrations/2026_04_24_160846_update_user_department_and_roles.php
+++ vicmis-backend/database/migrations/2026_04_24_160846_update_user_department_and_roles.php
@@ -8,11 +8,11 @@ return new class extends Migration
     public function up()
     {
         // 1. Change department from ENUM to a standard String/VARCHAR
-        //DB::statement("ALTER TABLE users MODIFY COLUMN department VARCHAR(255) NOT NULL DEFAULT 'Unassigned'");
+        DB::statement("ALTER TABLE users MODIFY COLUMN department VARCHAR(255) NOT NULL DEFAULT 'Unassigned'");
 
         // 2. Add 'department_employee' to the existing role ENUM
         // Make sure to include all your existing roles here plus the new one
-        //DB::statement("ALTER TABLE users MODIFY COLUMN role ENUM('super_admin', 'manager', 'dept_head', 'sales_employee', 'logistics_employee', 'engineering_employee', 'accounting_employee', 'department_employee') NOT NULL DEFAULT 'department_employee'");
+        DB::statement("ALTER TABLE users MODIFY COLUMN role ENUM('super_admin', 'manager', 'dept_head', 'sales_employee', 'logistics_employee', 'engineering_employee', 'accounting_employee', 'department_employee') NOT NULL DEFAULT 'department_employee'");
     }
 
     public function down()
